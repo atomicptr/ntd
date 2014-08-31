4d74fb22e96119c515c79260a430b1cbaea4d713
Christopher Kaster <ikasoki@gmail.com>
2014-08-31 16:09:53 +0200

    add 2 more test cases for curry, move test unit creation to curry_test.hpp

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,14 +1,7 @@
-#include <mavis/mavis.hpp>
-
 #include "tests/curry_test.hpp"
 
 int main() {
-    // curry tests
-    auto curry_tests = mavis::create_test_unit("curry tests");
-
-    curry_tests.add_test(test_curry_add);
-
-    curry_tests.run_tests();
+    run_curry_tests();
 
     return 0;
 }
--- tests/tests/curry_test.hpp
+++ tests/tests/curry_test.hpp
@@ -4,7 +4,13 @@
 #include <ntd/curry.hpp>
 #include <mavis/mavis.hpp>
 
-auto test_curry_add() {
+// used for test_curry_function and test_curry_curry
+int add(int x, int y) {
+    return x + y;
+}
+
+// test cases
+auto test_curry_lambda() {
     auto sum = [](auto first, auto... rest) {
         auto sum = first;
 
@@ -18,6 +24,35 @@ auto test_curry_add() {
     auto func = ntd::curry(sum, 1, 2, 3, 4);
 
     return mavis_assert_equals(sum(1, 2, 3, 4, 5, 5, 22), func(5, 5, 22));
+}
+
+auto test_curry_function() {
+    auto add40 = ntd::curry(add, 40);
+
+    return mavis_assert_equals(42, add40(2));
+}
+
+auto test_curry_curry() {
+    // to curry curry I need to put it into another function
+    auto curry = [](auto... args) {
+        return ntd::curry(args...);
+    };
+
+    auto curried_curry = ntd::curry(curry, add, 4);
+
+    return mavis_assert_equals(10, curried_curry(6)());
+}
+
+
+void run_curry_tests() {
+    // curry tests
+    auto curry_tests = mavis::create_test_unit("curry tests");
+
+    curry_tests.add_test(test_curry_lambda);
+    curry_tests.add_test(test_curry_function);
+    curry_tests.add_test(test_curry_curry);
+
+    curry_tests.run_tests();
 }
 
 #endif
