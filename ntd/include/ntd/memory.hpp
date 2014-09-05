fc7d2ea024a138cbd3b97c1926106c89bdaad11d
Christopher Kaster <ikasoki@gmail.com>
2014-09-05 15:11:13 +0200

    add documentation, close #1

--- ntd/include/ntd/memory.hpp
+++ ntd/include/ntd/memory.hpp
@@ -3,6 +3,17 @@
 
 namespace ntd {
 
+    /**
+        This function can turn a C construct and destruct combo-function into a smart pointer.
+
+        <b>Example</b> <pre>auto window = ntd::make_smart<Window>(Lib_CreateWindow, Lib_DestroyWindow, "Title", x, y);</pre>
+
+        @param creator_function A function (object_type* function()) which creates the object.
+        @param deleter_function A function (void function(object_type*)) which deletes the object.
+        @param arguments... The arguments passed into the creator function
+
+        @return A std::shared_ptr to the object created with the creator_function
+    */
     template<typename type, typename creator_function, typename deleter_function, typename... arguments>
     auto make_smart(creator_function c, deleter_function d, arguments... args) {
         return std::shared_ptr<type>(c(std::forward<arguments>(args)...), d);
--- ntd/include/ntd/string.hpp
+++ ntd/include/ntd/string.hpp
@@ -6,7 +6,38 @@
 #include <sstream>
 
 namespace ntd {
+
+    /**
+        Replaces all occurrences of a specified substring with another string.
+
+        <b>Example</b>
+
+        <pre>
+        auto text = ntd::replace("Java is awesome", "Java", "C++14"); // text = "C++14 is awesome"
+        </pre>
+
+        @param string The string where you want to replace things from
+        @param from The substring you want to replace
+        @param to The string which you want to insert instead of the "from" string mentioned above.
+
+        @return Returns the first argument with all occurences of the specified "from" substring replaced with "to".
+    */
     std::string replace(const std::string, const std::string, const std::string);
+
+    /**
+        Splits a string by a specified delimeter
+
+        <b>Example</b>
+
+        <pre>
+        auto words = ntd::replace("Lorem ipsum dolor sit amet", ' '); // words = {"Lorem", "ipsum", ...}
+        </pre>
+
+        @param string Your base string
+        @param delimeter The delimiter with which you'll split the string
+
+        @return Returns a std::vector<std::string> which contains several substrings based on your delimeter.
+    */
     std::vector<std::string> split(const std::string, const char);
 }
 
