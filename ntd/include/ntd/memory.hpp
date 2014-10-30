20b727f0506915e08f07c017731365e7d0d9127b
Christopher Kaster <ikasoki@gmail.com>
2014-10-30 18:57:42 +0100

    rename template parameter

--- ntd/include/ntd/memory.hpp
+++ ntd/include/ntd/memory.hpp
@@ -10,15 +10,15 @@ namespace ntd {
 
         <b>Example</b> <pre>auto window = ntd::make_smart<Window>(Lib_CreateWindow, Lib_DestroyWindow, "Title", x, y);</pre>
 
-        @param creator_function A function (object_type* function()) which creates the object.
-        @param deleter_function A function (void function(object_type*)) which deletes the object.
-        @param arguments... The arguments passed into the creator function
+        @param Constructor A function (object_type* function()) which creates the object.
+        @param Destructor A function (void function(object_type*)) which deletes the object.
+        @param Arguments... The Arguments passed into the creator function
 
-        @return A std::shared_ptr to the object created with the creator_function
+        @return A std::shared_ptr to the object created with the Constructor
     */
-    template<typename type, typename creator_function, typename deleter_function, typename... arguments>
-    auto make_smart(creator_function c, deleter_function d, arguments... args) {
-        return std::shared_ptr<type>(c(std::forward<arguments>(args)...), d);
+    template<typename T, typename Constructor, typename Destructor, typename... Arguments>
+    auto make_smart(Constructor construct, Destructor destroy, Arguments... args) {
+        return std::shared_ptr<T>(construct(std::forward<Arguments>(args)...), destroy);
     }
 }
 
